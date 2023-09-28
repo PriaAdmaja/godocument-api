9f38979516105a69509b7fb82dfc1418dac2cdee
PriaAdmaja <priaadmaja@gmail.com>
2023-09-28 07:09:16 +0700

    add authentication middleware

---
+++ src/middleware/authentication.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+const redisClient = require('../configs/redis');
+
+const checkToken = async (req, res, next) => {
+    //get token
+    const bearerToken = req.header("Authorization");
+    if(!bearerToken) {
+        return res.status(401).json({
+            msg: "Please login first!"
+        })
+    };
+    const token = bearerToken.split(" ")[1];
+    
+    //check token in redis
+    await redisClient.connect();
+    const blTokenList = await redisClient.get(`bl_${token}`);
+    if(blTokenList) {
+        return res.status(401).json({
+            msg: "Invalid token"
+        });
+    };
+    await redisClient.disconnect();
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+        if (err && err.name) {
+            return res.status(403).json({
+                msg: err.message
+            });
+        };
+        if (err) {
+            return res.status(403).json({
+                msg: "Internal server error"
+            });
+        };
+        req.authInfo = payload;
+        req.token = token;
+        next()
+    });
+}
+
+module.exports = {
+    checkToken
+}
