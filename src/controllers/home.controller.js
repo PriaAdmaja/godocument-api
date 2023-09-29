7a8656d13afbf93184aac19e21a12423492763e8
PriaAdmaja <priaadmaja@gmail.com>
2023-09-29 16:53:20 +0700

    edit home

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,6 +1,8 @@
 const home = (req, res) => {
     res.json({
-        msg: `Welcome to GoDocument API`
+        msg: `Welcome to GoDocument API`,
+        author: 'M. Pria Admaja',
+        source: 'https://github.com/PriaAdmaja/godocument-api'
     });
 };
 
