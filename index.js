57d976c6bdb4711c2ded5c42e4341474559030f3
PriaAdmaja <priaadmaja@gmail.com>
2023-09-30 14:44:16 +0700

    add bodyparser and morgan

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require("morgan");
 require('dotenv').config();
 const PORT = 8080;
 
 const app = express();
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
 
 const masterRouter = require('./src/routes/index')
 app.use(masterRouter);
